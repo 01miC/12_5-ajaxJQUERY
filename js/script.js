3b797588c1a704c7d9d49c8152459a1a46e29fd2
Michal Dokupil <michal@dokupil.pl>
2018-01-17 20:51:56 +0100

    No style tweet app

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function(){
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
+
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    if (tweetText.length > 280) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }
+};
+
